0faa4d92dbe0d27d2e7bde8a4ef713be29f61038
Fernanda Assis <fernandamassis@outlook.com>
2020-07-13 12:45:42 -0300

    Enter

--- src/main.js
+++ src/main.js
@@ -15,6 +15,18 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    }else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -23,20 +35,29 @@ class App {
     if(repoInput.length === 0) 
       return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    const {name, description, html_url, owner: {avatar_url}} = response.data;
-    
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
+    try{
+      const response = await api.get(`/repos/${repoInput}`);
 
-    this.inputEl.value = '';
+      const {name, description, html_url, owner: {avatar_url}} = response.data;
+      
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
 
-    this.render();
+      this.inputEl.value = '';
+
+      this.render();
+    }catch (err) {
+      alert('O repositório não existe');
+    }
+
+    this.setLoading(false);
+
   }
   render() {
     this.listEl.innerHTML = '';
